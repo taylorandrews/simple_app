e536bd8e564d1a71247aa8ac00371e297bb737a9
Taylor Andrews <twandrews@icloud.com>
2021-11-19 17:41:08 -0700

    Initial Commit

---
+++ simple_app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  simple_app
+//
+//  Created by Taylor Andrews on 11/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ simple_app/simple_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  simple_appApp.swift
+//  simple_app
+//
+//  Created by Taylor Andrews on 11/19/21.
+//
+
+import SwiftUI
+
+@main
+struct simple_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
